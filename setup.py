bd0626ea477af2651de5e7fdea8d43153d45c146
Jan Vlk <jan.vlk@mensa.cz>
2025-09-10 12:48:02 +0200

    prepare ROS2 package

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import find_packages, setup
+
+package_name = "mission_reporter"
+
+setup(
+    name=package_name,
+    version="1.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        (os.path.join("share", package_name), ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="vlkjan6",
+    maintainer_email="vlkjan6@fel.cvut.cz",
+    description="TODO: Package description",
+    license="BSD-3-Clause",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [],
+    },
+)
